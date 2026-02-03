702aca4bac6b60559887cea530688ffa13a392c8
Rachel S. Delos Reyes <159454163+dlsrys@users.noreply.github.com>
2026-02-03 10:42:40 +0800

    Database and live video button

---
+++ VehicleCounting/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VehicleCounting.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id{ get; set; }
+        public int Time { get; set; }
+        public int VideoName { get; set; }
+        public int Bus { get; set; }
+        public int Jeep { get; set; }
+        public int Tricycle { get; set; }
+        public int Van { get; set; }
+        public int Truck { get; set; }
+        public int Car { get; set; }
+    }
+}
